d09ead0d027dee1b9533e2cf519b33a836e5470f
salsabileslam <salsabilelshiekh6@gmail.com>
2024-01-07 20:29:59 +0200

    Problem 1512 number of good pair

---
+++ 1512.number_of_good_pair.dart
@@ -0,0 +1,18 @@
+void main() {
+  Solution s = Solution();
+  print(s.numIdenticalPairs([1, 2, 3]));
+}
+
+class Solution {
+  int numIdenticalPairs(List<int> nums) {
+    int count = 0;
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[i] == nums[j]) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+}
