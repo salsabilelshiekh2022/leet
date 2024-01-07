1ba9ff2f785bbdf0b2c02fe561c8703e02c9514e
salsabileslam <salsabilelshiekh6@gmail.com>
2024-01-07 17:36:02 +0200

    Problem 242 valid anagram

---
+++ 242.valid_anagram.dart
@@ -0,0 +1,19 @@
+void main() {
+  Solution s = Solution();
+  print(s.isAnagram("rat", "car"));
+}
+
+class Solution {
+  bool isAnagram(String s, String t) {
+    if (s.length != t.length) return false;
+    List<String> sList = s.split('');
+    List<String> tList = t.split('');
+    sList.sort();
+    tList.sort();
+
+    for (var i = 0; i < sList.length; i++) {
+      if (sList[i] != tList[i]) return false;
+    }
+    return true;
+  }
+}
